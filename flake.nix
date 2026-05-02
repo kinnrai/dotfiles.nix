f4bd067aac510e719cd1ad362dca789c2edfdbc9
kinnrai <me@kinnrai.com>
2026-05-02 23:45:39 +0800

    feat(homebrew): add declarative brew and cask management

--- flake.nix
+++ flake.nix
@@ -6,13 +6,69 @@
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+    homebrew-aninsomniacy-motrix-next = {
+      url = "github:AnInsomniacy/homebrew-motrix-next";
+      flake = false;
+    };
+    homebrew-antoniorodr-memo = {
+      url = "github:antoniorodr/homebrew-memo";
+      flake = false;
+    };
+    homebrew-betterdiscord-tap = {
+      url = "github:betterdiscord/homebrew-tap";
+      flake = false;
+    };
+    homebrew-farion1231-ccswitch = {
+      url = "github:farion1231/homebrew-ccswitch";
+      flake = false;
+    };
+    homebrew-felixkratz-formulae = {
+      url = "github:FelixKratz/homebrew-formulae";
+      flake = false;
+    };
+    homebrew-jundot-omlx = {
+      url = "github:jundot/omlx";
+      flake = false;
+    };
+    homebrew-lihaoyun6-tap = {
+      url = "github:lihaoyun6/homebrew-tap";
+      flake = false;
+    };
+    homebrew-mediosz-tap = {
+      url = "github:mediosz/homebrew-tap";
+      flake = false;
+    };
+    homebrew-nikitabobko-tap = {
+      url = "github:nikitabobko/homebrew-tap";
+      flake = false;
+    };
+    homebrew-productdevbook-tap = {
+      url = "github:productdevbook/homebrew-tap";
+      flake = false;
+    };
+    homebrew-rafaelswi-menubarusb = {
+      url = "github:rafaelswi/homebrew-menubarusb";
+      flake = false;
+    };
+    homebrew-steipete-tap = {
+      url = "github:steipete/homebrew-tap";
+      flake = false;
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }: {
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, ... }: {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#Tomrins-MacBook-Pro
     darwinConfigurations."Tomrins-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-      specialArgs = { inherit self; };
+      specialArgs = { inherit self inputs; };
       modules = [
         nix-homebrew.darwinModules.nix-homebrew
         ./modules/default.nix
--- modules/homebrew/nix-homebrew.nix
+++ modules/homebrew/nix-homebrew.nix
@@ -1,8 +1,30 @@
+{ config, inputs, ... }:
+
 {
   nix-homebrew = {
     enable = true;
     enableRosetta = false;
     user = "kinnrai";
     autoMigrate = true;
+    mutableTaps = false;
+
+    taps = {
+      "homebrew/homebrew-core" = inputs.homebrew-core;
+      "homebrew/homebrew-cask" = inputs.homebrew-cask;
+      "AnInsomniacy/homebrew-motrix-next" = inputs.homebrew-aninsomniacy-motrix-next;
+      "antoniorodr/homebrew-memo" = inputs.homebrew-antoniorodr-memo;
+      "betterdiscord/homebrew-tap" = inputs.homebrew-betterdiscord-tap;
+      "farion1231/homebrew-ccswitch" = inputs.homebrew-farion1231-ccswitch;
+      "FelixKratz/homebrew-formulae" = inputs.homebrew-felixkratz-formulae;
+      "jundot/homebrew-omlx" = inputs.homebrew-jundot-omlx;
+      "lihaoyun6/homebrew-tap" = inputs.homebrew-lihaoyun6-tap;
+      "mediosz/homebrew-tap" = inputs.homebrew-mediosz-tap;
+      "nikitabobko/homebrew-tap" = inputs.homebrew-nikitabobko-tap;
+      "productdevbook/homebrew-tap" = inputs.homebrew-productdevbook-tap;
+      "rafaelswi/homebrew-menubarusb" = inputs.homebrew-rafaelswi-menubarusb;
+      "steipete/homebrew-tap" = inputs.homebrew-steipete-tap;
+    };
   };
+
+  homebrew.taps = builtins.attrNames config.nix-homebrew.taps;
 }
