9e22c4ea744a6508dfb9f836e5df527c39e6c90c
kinnrai <me@kinnrai.com>
2026-05-09 23:35:10 +0800

    feat(git): manage config with home-manager

---
+++ modules/home-manager/git.nix
@@ -0,0 +1,55 @@
+{ userHome, ... }:
+
+let
+  userName = "kinnrai";
+  userEmail = "me@kinnrai.com";
+  signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMGFYbXgap+CmxxYRjrV8VEEkTSyTKCoNgkAYDojQbdS personal";
+in
+{
+  programs.git = {
+    enable = true;
+    package = null;
+
+    signing = {
+      key = signingKey;
+      format = "ssh";
+    };
+
+    ignores = [
+      # macOS
+      ".DS_Store"
+      ".AppleDouble"
+      ".LSOverride"
+      "._*"
+
+      # Linux
+      "*~"
+      ".fuse_hidden*"
+
+      # Windows
+      "Thumbs.db"
+      "Desktop.ini"
+      "$RECYCLE.BIN/"
+    ];
+
+    settings = {
+      user = {
+        name = userName;
+        email = userEmail;
+      };
+
+      gpg.ssh.allowedSignersFile = "${userHome}/.ssh/allowed_signers";
+
+      commit = {
+        gpgSign = true;
+        verbose = true;
+      };
+
+      pull.rebase = true;
+
+      init.defaultBranch = "master";
+
+      push.autoSetupRemote =true;
+    };
+  };
+}
