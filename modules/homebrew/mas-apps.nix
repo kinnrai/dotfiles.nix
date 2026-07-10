5076bf9c6a3aa60489db06fe5607824fc8ab38ea
kinnrai <me@kinnrai.com>
2026-07-11 01:17:24 +0800

    feat(homebrew): add AndroMeld, GlossOne, PicOne, and TransOne to App Store apps

--- modules/homebrew/mas-apps.nix
+++ modules/homebrew/mas-apps.nix
@@ -1,17 +1,21 @@
 {
   "AdGuard Mini" = 1440147259;
+  "AndroMeld" = 6762439757;
   "Bitwarden" = 1352778147;
   "Control Panel for Twitter" = 1668516167;
   "Control Panel for YouTube" = 6478456678;
   "CotEditor" = 1024640650;
   "Draw Things" = 6444050820;
+  "GlossOne" = 6782971706;
   "Klack" = 6446206067;
   "Microsoft Excel" = 462058435;
   "Microsoft PowerPoint" = 462062816;
   "Microsoft Word" = 462054704;
   "Noir" = 1592917505;
+  "PicOne" = 6778935489;
   "Refined GitHub" = 1519867270;
   "Tampermonkey" = 6738342400;
+  "TransOne" = 6781136563;
   "Trancy ET- AI Translator" = 6475386403;
   "Vimlike" = 1584519802;
   "Xcode" = 497799835;
